b7c3708798473be83bc22c6239596685af87c575
Christina Mikheyeva <c.mikheyeva@gmail.com>
2024-05-10 11:16:14 +0200

    First task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
-import Profile from './components/Profile/Profile'
-import userData from "./userData.json";
+import './App.css';
+import Profile from './components/Profile/Profile';
+import userData from './userData.json';
 
-function App() {
-console.log (userData )
+const App = () => {
   return (
-    <div>
-      {userData.map((el) => { return <Profile name={el.username}/>})}
-    </div>
-  )
-}
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
 
-export default App
+export default App;
